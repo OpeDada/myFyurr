7d0560c414c2a69b124a49bf6ad81ce218e86b91
OpeDada <opedada@gmail.com>
2022-08-18 14:46:41 +0900

    implement artist and venue search methods

--- app.py
+++ app.py
@@ -60,14 +60,18 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
+
+  #retrieve the search term from the request
+  search_term = request.form.get('search_term')
+
+  #do a query filter with it and fetch all records that match the search term
+  venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
+
+  #build the response
+  response = {}
+  response['count'] = len(venues)
+  response['data'] = venues
+
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -301,14 +305,18 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+
+  #retrieve the search term from the request
+  search_term = request.form.get('search_term')
+
+  #do a query filter with it and fetch all records that match the search term
+  artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+
+  #build the response
+  response = {}
+  response['count'] = len(artists)
+  response['data'] = artists
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
