340d1d984e5fec97acfebb479cbab939a6623b03
OpeDada <opedada@gmail.com>
2022-08-19 18:37:30 +0900

    fix migration issues

---
+++ migrations/versions/08f56db3df42_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 08f56db3df42
+Revises: 
+Create Date: 2022-08-19 18:31:47.605201
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '08f56db3df42'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
